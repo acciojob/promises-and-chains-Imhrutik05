216cd7e820413d5b838064e0dbb2cc8cc30ddde4
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-28 16:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.getElementById('userForm').addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	const age = document.querySelector('#age').value.trim();
+	const name = document.querySelector('#name').value.trim();
+
+	if (!name || !age){
+		alert("Please enter valid details.");
+		return;
+	}
+
+	// creating promise
+	const checkAge = new Promise ((resolve, reject) => {
+		setTimeout(() => {
+			if(parseInt(age) > 18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			} else {
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		}, 4000);
+	});
+
+	// handling that promise
+	checkAge
+		.then(message => alert(message))
+		.catch(error => alert(error));
+
+});
